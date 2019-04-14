483ba11fc8b475e34e377b78ffa68c15ba64350c
dozer111 <alkhonko@gmail.com>
2019-04-14 17:12:53 +0300

    fix make-back script

--- quest1/make-backup.sh
+++ quest1/make-backup.sh
@@ -5,10 +5,13 @@
 MY_PROJECT_DIRECTORY="/var/www/itea/quest1"
 FINAL_FILENAME="$MY_PROJECT_DIRECTORY/dump.sql"
 
-
+# По идее, данный проект УЖЕ должен быть склонирован с гита, если его нет, то сначала просто создаю под него директорию
 if [ ! -d "$MY_PROJECT_DIRECTORY" ]
 then
+    echo "Нету проекта/некорректная директория."
     mkdir -p "$MY_PROJECT_DIRECTORY"
+    echo "Создали новую директорию $MY_PROJECT_DIRECTORY, в которую нужно склонировать проект и ещё раз запустить этот скрипт"
+    exit 1
 fi
 
 touch $FINAL_FILENAME;
--- quest1/restore-backup.sh
+++ quest1/restore-backup.sh
@@ -5,17 +5,20 @@ set -e
 #Заменить в случае необходимости
 MY_PROJECT_DIRECTORY="/var/www/itea/quest1"
 
-
-
-BACKUP_DIRECTORY="backend/web/uploads"
-
-
 if [[ ! $1 =~ ^[0-9]{2}-[0-9]{2}-[0-9]{4}$ ]]
   then
      echo "Введенное значение не валидно, скрипт должен вызыватся в формате ./scriptName.sh d-m-Y"
     exit 1
 fi
 
+if [ ! -d "$MY_PROJECT_DIRECTORY" ]
+then
+    echo "Некорректная рабочая директория. Execute файл make-backup.sh"
+    exit 1
+fi
+
+
+BACKUP_DIRECTORY="backend/web/uploads"
 DUMP_DIRECTORY="$MY_PROJECT_DIRECTORY/$BACKUP_DIRECTORY/$1"
 
 
