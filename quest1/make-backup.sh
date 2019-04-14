c503fb0272438dee4078facda98f677aa3c112a8
dozer111 <alkhonko@gmail.com>
2019-04-14 16:41:12 +0300

    fix make-back script

--- quest1/make-backup.sh
+++ quest1/make-backup.sh
@@ -2,10 +2,14 @@
 
 
 #Заменить в случае необходимости
-MY_PROJECT_DIRECTORY="/var/www/itea/quest1"
+MY_PROJECT_DIRECTORY="/var/www/itea/quest1/ttt/xxxx"
 FINAL_FILENAME="$MY_PROJECT_DIRECTORY/dump.sql"
 
 
+if [ ! -d "$MY_PROJECT_DIRECTORY" ]
+then
+    mkdir -p "$MY_PROJECT_DIRECTORY"
+fi
 
 touch $FINAL_FILENAME;
 
